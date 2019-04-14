01a8a774a06a95b6b0d5be649915bcc80c265093
Adam Pod <adam.podoxin@gmail.com>
2019-04-13 20:50:28 -0700

    Create path and path follower

---
+++ Assets/Scripts/Pathfinding/FollowPath.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    [SerializeField]
+    private Path path;
+
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private Vector2 direction;
+
+    private Transform nextNode;
+    private int nextNodeIndex = 0;
+
+    private void Start()
+    {
+        nextNode = path.Nodes[0];
+
+        CalculateDirection();
+    }
+
+    private void Update()
+    {
+        MoveToNode(nextNode);
+        CheckPathProgress();
+    }
+
+    private void CalculateDirection()
+    {
+        direction = (nextNode.position - transform.position).normalized;
+    }
+
+    private bool CalculateProximityToNextNode()
+    {
+        return (nextNode.position - transform.position).magnitude < 0.1f;
+    }
+
+    private void SetNextNode()
+    {
+        nextNodeIndex++;
+        nextNode = path.Nodes[nextNodeIndex];
+    }
+
+    private void MoveToNode(Transform node)
+    {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+    private void CheckPathProgress()
+    {
+        if (CalculateProximityToNextNode())
+        {
+            if (nextNodeIndex < path.Nodes.Count - 1)
+            {
+                SetNextNode();
+                CalculateDirection();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Pathfinding/Path.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public List<Transform> Nodes { get; } = new List<Transform>();
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Nodes.Add(child);
+        }
+    }
+
+    public Transform GetClosestNodeFromPosition(Vector3 position)
+    {
+        Transform closestNode = Nodes[0];
+        float closestMagnitude = (position - closestNode.position).magnitude;
+
+        foreach (Transform node in Nodes)
+        {
+            if ((position - node.position).magnitude < closestMagnitude)
+            {
+                closestNode = node;
+                closestMagnitude = (position - node.position).magnitude;
+            }
+        }
+
+        return closestNode;
+    }
+
+    public Transform GetClosestNodeFromPosition(Vector3 position, bool includeFirstNode)
+    {
+        Transform closestNode;
+
+        if (includeFirstNode) closestNode = Nodes[0];
+        else closestNode = Nodes[1];
+
+        float closestMagnitude = (position - closestNode.position).magnitude;
+
+        foreach (Transform node in Nodes)
+        {
+            if (!includeFirstNode)
+            {
+                if (node.Equals(Nodes[0])) continue;
+            }
+
+            if ((position - node.position).magnitude < closestMagnitude)
+            {
+                closestNode = node;
+                closestMagnitude = (position - node.position).magnitude;
+            }
+        }
+
+        return closestNode;
+    }
+
+    public Transform GetClosestNodeFromPosition(Vector3 position, Transform excludeNode)
+    {
+        Transform closestNode = Nodes[0];
+        float closestMagnitude = (position - closestNode.position).magnitude;
+
+        foreach (Transform node in Nodes)
+        {
+            if (node.Equals(excludeNode)) continue;
+
+            if ((position - node.position).magnitude < closestMagnitude)
+            {
+                closestNode = node;
+                closestMagnitude = (position - node.position).magnitude;
+            }
+        }
+
+        return closestNode;
+    }
+}
